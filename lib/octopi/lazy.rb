89d6b5948d10437a33df39f4d8386866e2974895
Run Paint Run Run <runrun@runpaint.org>
2009-04-24 19:21:24 +0100

    Revert "This reverts commit 486914f4cf1ce746ac32efae786c134349d97836."
    This reverts commit bcd02a5306c8ce148d756ea2c1591e72c258773b.

--- lib/octopi/lazy.rb
+++ lib/octopi/lazy.rb
@@ -7,16 +7,25 @@ module Octopi
       @worker = nil
       @args = args
     end
+    
+    def self.method_missing(method,*args)
+      $stderr.puts "Lazy does not handle missing class methods"
+      super method, *args
+    end
 
     def method_missing(method,*args)  
-      self.init_worker
-      if @worker.class.respond_to? method
-        @args << args
-        @worker.class.send(method,*@args)
+      if self.worker_class.respond_to? method
+        @args.concat args
+        self.worker_class.send(method,@args.first)
       else 
+        self.init_worker
         @worker.send(method,*args)
       end 
     end
+    
+    def worker_class
+      Kernel.const_get self.class.to_s.sub(/^Octopi::Lazy/,'')
+    end  
 
   end    
 end
