c28041c84a37a3ead6357a23b3fcdad80e8416f4
Run Paint Run Run <runrun@runpaint.org>
2009-04-24 21:24:41 +0100

    Add .search to return Array of LazyUsers.

--- lib/octopi/lazyuser.rb
+++ lib/octopi/lazyuser.rb
@@ -1,13 +1,21 @@
 module Octopi
   class LazyUser < Lazy
     
-    def initialize(username)
-      @username = username
-      super username
+    def initialize(arg)
+      @username = arg
+      super User === arg ? arg.name : @username
     end
 
-    def init_worker  
-      @worker ||= User.find(@username)
+    def init_worker
+      if User === @username
+        @worker ||= @username
+      else 
+        @worker ||= User.find(@username)
+      end  
+    end
+  
+    def self.search(query)
+      self.find_all(query).map{|u| LazyUser.new u}
     end
 
   end    
