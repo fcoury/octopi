d7234f3fc22719be26f3f7104719f21a655fb208
Ryan Bigg <radarlistener@gmail.com>
2011-09-24 18:12:04 -0500

    Add ability to update a comment

--- spec/octopi/gist/comment_spec.rb
+++ spec/octopi/gist/comment_spec.rb
@@ -2,19 +2,20 @@ require 'spec_helper'
 
 describe Octopi::Gist::Comment do
   let(:gist) { Octopi::Gist.find(1236602) }
+  let(:comment_url) { base_url + "gists/comments/52291" }
   before do
     authenticated_api_stub("gists/1236602")
     authenticated_api_stub("gists/1236602/comments")
   end
 
   it "gets a single comment" do
-    url = base_url + "gists/comments/52291"
-    stub_request(:get, url).to_return(fake("gists/comments/52291"))
+    stub_request(:get, comment_url).to_return(fake("gists/comments/52291"))
     Octopi::Gist::Comment.find(52291)
-    WebMock.should have_requested(:get, url)
+    WebMock.should have_requested(:get, comment_url)
   end
 
   context "authenticated" do
+    let(:comment_url) { authenticated_base_url + "gists/comments/52291" }
     before do
       Octopi.authenticate!(:username => "radar", :password => "password")
     end
@@ -25,6 +26,13 @@ describe Octopi::Gist::Comment do
       comment = gist.comments.create!(:body => "This is a brand new comment!")
       comment.is_a?(Octopi::Gist::Comment).should be_true
       WebMock.should have_requested(:post, url)
+    end
+    
+    it "updates a comment" do
+      stub_request(:post, comment_url).to_return(fake("gists/comments/update"))
+      comment = Octopi::Gist::Comment.find(52291)
+      comment.update(:body => "This is new content")
+      WebMock.should have_requested(:post, comment_url)
     end
   end
 end
