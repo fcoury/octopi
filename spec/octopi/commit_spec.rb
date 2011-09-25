afc9410d57f2a2b0e6583befbc05851c76ba36c7
Ryan Bigg <radarlistener@gmail.com>
2011-09-25 16:05:04 -0500

    Add ability to find a short sha commit

--- spec/octopi/commit_spec.rb
+++ spec/octopi/commit_spec.rb
@@ -45,4 +45,12 @@ describe Octopi::Commit do
     commit = repo.commits.find("38b679a92a49bb49a72e57d99639e26830b7792b")
     commit.message.should == "Merge pull request #68 from nithinbekal/patch-1\n\nFix the incorrect example 2 for explicit authentication. Refs #59"
   end
+  
+  it "finds a single commit by short sha" do
+    api_stub("repos/fcoury/octopi/commits")
+    api_stub("repos/fcoury/octopi/commits/38b679")
+    commit = repo.commits.find("38b679")
+    commit.message.should == "Merge pull request #68 from nithinbekal/patch-1\n\nFix the incorrect example 2 for explicit authentication. Refs #59"
+    commit.sha.should == "38b679a92a49bb49a72e57d99639e26830b7792b"
+  end
 end
