2295047bb2ee42d80be1510d894e5319504a4a17
Ryan Bigg <radarlistener@gmail.com>
2011-09-26 20:47:10 -0500

    Add pending tests for downloads

---
+++ spec/octopi/download_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Octopi::Download do
+  it "can retreive downloads"
+  it "can get a single download"
+  it "can begin to create a download"
+  it "can delete a download"
+end
